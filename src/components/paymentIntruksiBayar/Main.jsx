d63599c71cb9a98a87a659c2cac2381a7abd20da
Fajar Nurdiantono <nurdiantonoff@gmail.com>
2023-07-14 20:46:23 +0700

    fixing src img

--- src/components/paymentIntruksiBayar/Main.jsx
+++ src/components/paymentIntruksiBayar/Main.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import IntruksiPembayaran from './InstruksiPembayaran';
 import MetodePembayaran from './MetodePembayaran';
 import Verifikasi from './Verifikasi';
+import Kanan from '../../assets/Kanan.jpeg';
+import Kiri from '../../assets/Kiri.jpeg';
 
 function Main() {
   return (
     <div className="bg-main h-full w-full font-Mulish inline-block">
       <div className="flex ml-[40px] mt-10">
-        <img src={require('../../assets/Kiri.jpeg')} alt="Kiri" className="mr-2" />
+        <img src={Kiri} alt="Kiri" className="mr-2" />
         <p className="font-500 text-xs text-subdued">Checkout</p>
-        <img src={require('../../assets/Kanan.jpeg')} alt="Kanan" className="mx-3" />
+        <img src={Kanan} alt="Kanan" className="mx-3" />
         <p className="font-700 text-xs text-biru">Instruksi Bayar</p>
       </div>
       <div className="font-700 text-[28px] ml-10 mt-5">
